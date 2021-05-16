7d5c477e267fad5ad3f3ff7c108d4f9c1bccfc07
pravinbangar21 <54072844+pravinbangar21@users.noreply.github.com>
2021-05-16 14:06:05 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "pipeline-artifacts" {
   bucket = "my-code-pipeline-artifacts-p1"
   acl = "private"
 }
+
+resource "aws_s3_bucket" "sample2" {
+  bucket = "test2"
+  acl = "private"
+}
