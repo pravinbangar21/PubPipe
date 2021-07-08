c176515ad98984f84dad95cf67488b5dd6c5e767
pravinbangar21 <54072844+pravinbangar21@users.noreply.github.com>
2021-07-08 23:32:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,5 @@
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
-/*
-provider "aws" {
-  access_key = "var.AWS_ACCESS_KEY"
-  secret_key = "var.AWS_SECRET_KEY"
-  region = "ap-south-1"
-}
-*/
