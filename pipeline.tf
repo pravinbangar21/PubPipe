66868016e24561295168b53a2d4232658fcd2e75
pravinbangar21 <54072844+pravinbangar21@users.noreply.github.com>
2021-07-15 01:46:23 +0530

    Update pipeline.tf

--- pipeline.tf
+++ pipeline.tf
@@ -11,7 +11,7 @@ resource "aws_codebuild_project" "tf-plan" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image = "hashicorp/terraform:0.14.2"
+    image = "hashicorp/terraform:0.14.3"
     type = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential {
@@ -41,7 +41,7 @@ resource "aws_codebuild_project" "tf-apply" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image = "hashicorp/terraform:0.14.2"
+    image = "hashicorp/terraform:0.14.3"
     type = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential {
